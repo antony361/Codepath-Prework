17ed9863405b8b35b1683cff768e7cd7a17bf37e
antony361 <antoniorazojr361@gmail.com>
2022-02-04 20:41:29 -0800

    Made keyboard first responder and added nicer theme

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.billAmountTextField.becomeFirstResponder()
     }
 
     @IBAction func calculateTip(_ sender: Any) {
